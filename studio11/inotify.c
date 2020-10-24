02b8c22ea518ea6c6b34af36446c78067939964a
bingxinliu <bingxin.liu@wustl.edu>
2020-10-24 02:19:30 -0500

    studio11 q4

--- studio11/inotify.c
+++ studio11/inotify.c
@@ -3,12 +3,16 @@
 #include <string.h>
 #include <sys/inotify.h>
 
+#define BUF_LEN 1024
+
 const int num_expected_args = 2;
 
 int
 main(int argc, char *argv[])
 {
     int inotifyfd, wd;
+    char buf[BUF_LEN] __attribute__((aligned(__alignof__(struct inotify_event))));
+    ssize_t len, i;
 
     if ( argc != num_expected_args )
     {
@@ -31,6 +35,51 @@ main(int argc, char *argv[])
         return -1;
     }
     printf("Adding a watch successfully. The file descriptor is %d, and the command line argument is %s.\n", wd, argv[1]);
+
+    while (1)
+    {
+        i = 0;
+        len = read(inotifyfd, buf, BUF_LEN);
+        printf("===========================================================\n");
+        while ( i < len )
+        {
+            struct inotify_event *event = (struct inotify_event *) &buf[i];
+            printf("wd=%d mask=%d cookie=%d len=%d dir=%s\n",\
+                event->wd, event->mask, event->cookie, event->len, (event->mask & IN_ISDIR) ? "yes" : "no");
+            if ( event->len )
+            {
+                printf("name=%s\n", event->name);
+            }
+            if (event->mask & IN_ACCESS)
+                printf("File was accessed\n");
+            if (event->mask & IN_MODIFY)
+                printf("File was modified\n");
+            if (event->mask & IN_ATTRIB)
+                printf("Metadata changed\n");
+            if (event->mask & IN_CLOSE_WRITE)
+                printf("File opened for writing was closed.\n");
+            if (event->mask & IN_CLOSE_NOWRITE)
+                printf("File or directory not opened for writing was closed.\n");
+            if (event->mask & IN_OPEN)
+                printf("File or directory was opened.\n");
+            if (event->mask & IN_MOVED_FROM)
+                printf("File was move away from the watched directory.\n");
+            if (event->mask & IN_MOVED_TO)
+                printf("File was move into the watched directory.\n");
+            if (event->mask & IN_CREATE)
+                printf("File/directory created in watched directory\n");
+            if (event->mask & IN_DELETE)
+                printf("File or directory deleted from watched directory.\n");
+            if (event->mask & IN_DELETE_SELF)
+                printf("Watched file or directory was itself deleted.\n");
+            if (event->mask & IN_MOVE_SELF)
+                printf("Watched file or directory was itself moved.\n");
+
+            i += sizeof(struct inotify_event) + event->len;
+        }
+        
+    }
+    
 
     return 0;
 }
